fd45cbc9c0c4138c1a0bc6cc56fc33106370952b
Nakai <nakai.hamlin18@gmail.com>
2021-06-03 00:02:54 -0400

    Finish

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -3,7 +3,7 @@ module ReviewsHelper
         if params[:anime_id]
             content_tag(:h1, "Add a Review for #{review.anime.name} -  #{review.anime.category.name}")
         else
-          content_tag(:h1, "Create a review")
+          content_tag(:h1, "Create a Review")
         end
       end
 end
