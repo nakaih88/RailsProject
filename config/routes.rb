34aaa87e2805ff3699ba499694e752ee1b4fd7dd
Nakai <nakai.hamlin18@gmail.com>
2021-06-03 01:36:37 -0400

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,8 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new' 
   post '/signup' => 'users#create' 
   get '/logout' => 'sessions#destroy'
-  # get '/most_popular' => 'animes#most_popular' 
 
   get '/auth/:google_oauth2/callback' => 'sessions#google_omniauth_create'
- 
   
   resources :reviews
   resources :animes do
@@ -17,9 +15,4 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:show]
-  # resources :categories
-  # resources :animes
-  # resources :reviews
-  # resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
