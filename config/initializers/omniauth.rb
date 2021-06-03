7fb0f494328a10d2128341a9f04798337c04f89d
Nakai <nakai.hamlin18@gmail.com>
2021-06-03 11:53:16 -0400

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  OmniAuth.config.allowed_request_methods = [:post, :get]
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_KEY'], ENV['GOOGLE_CLIENT_SECRET'], {
-  scope: ['email',
-    'https://www.googleapis.com/auth/gmail.send'],
-    access_type: 'offline'}
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
+OmniAuth.config.allowed_request_methods = %i[get]
